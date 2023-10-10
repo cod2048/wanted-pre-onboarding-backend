3e6b96ff74aa35240c4474507f14a98644dd227d
cod2048 <cod2048@gmail.com>
2023-10-10 12:45:06 +0900

    채용공고 삭제 기능 추가

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -35,4 +35,22 @@ noticeController.updateNotice = async (req, res) => {
   }
 };
 
+//채용공고 삭제
+noticeController.deleteNotice = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const notice = await Notice.findByPk(id);
+
+    if (!notice) {
+      return res.status(404).json({ success: false, error: "Notice not found" });
+    }
+
+    await notice.destroy();
+    res.json({ success: true, message: "채용공고 삭제 성공" });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
 module.exports = noticeController;
--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -4,5 +4,6 @@ const noticeController = require('../controllers/noticeController');
 
 router.post('/notices', noticeController.createNotice);
 router.put('/notices/:id', noticeController.updateNotice);
+router.delete('/notices/:id', noticeController.deleteNotice);
 
 module.exports = router;
