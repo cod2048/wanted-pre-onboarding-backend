8f4267b14aa4cd494c716e57a3a96eeeca7968e5
cod2048 <cod2048@gmail.com>
2023-10-10 12:43:13 +0900

    채용공고 수정 기능 추가

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -2,10 +2,34 @@ const { Notice } = require('../models');
 
 const noticeController = {};
 
+//채용공고 생성
 noticeController.createNotice = async (req, res) => {
   try {
     const notice = await Notice.create(req.body);
     res.status(201).json({ success: true, data: notice });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+//채용공고 수정
+noticeController.updateNotice = async (req, res) => {
+  try {
+    const { id } = req.params;
+    let updateData = req.body;
+
+    if (updateData.companyId) {
+      delete updateData.companyId;
+    }
+
+    const notice = await Notice.findByPk(id);
+    if (!notice) {
+      return res.status(404).json({ success: false, error: "Notice not found" });
+    }
+
+    await notice.update(updateData);
+    res.json({ success: true, data: notice });
+
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const noticeController = require('../controllers/noticeController');
 
 router.post('/notices', noticeController.createNotice);
+router.put('/notices/:id', noticeController.updateNotice);
 
 module.exports = router;
