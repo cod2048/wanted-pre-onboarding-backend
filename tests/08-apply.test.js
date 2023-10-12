e03fc652f59c9528350fd69f5cd9a9b6581413d8
cod2048 <cod2048@gmail.com>
2023-10-12 11:41:19 +0900

    [생성]채용공고 지원 test code

---
+++ tests/08-apply.test.js
@@ -0,0 +1,53 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Apply To Notice', () => {
+  it('should apply to notice', async () => {
+    const response = await request(app)
+      .post('/apply')
+      .send({
+        user_id: 1,
+        notice_id: 1
+      });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data.noticeId).toBe(1);
+    expect(response.body.data.userId).toBe(1);
+  });
+
+  it('should not allow duplicate apply', async () => {
+    const response = await request(app)
+      .post('/apply')
+      .send({
+        user_id: 1,
+        notice_id: 1
+      });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe('이미 지원한 채용공고');
+  })
+
+  it('should not allow not exist apply', async () => {
+    const response = await request(app)
+      .post('/apply')
+      .send({
+        user_id: 1,
+        notice_id: 12
+      });
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body.error).toBe('insert or update on table \"Applies\" violates foreign key constraint \"Applies_noticeId_fkey\"');
+  });
+
+  it('should not allow not exist user', async () => {
+    const response = await request(app)
+      .post('/apply')
+      .send({
+        user_id: 12,
+        notice_id: 4
+      });
+
+    expect(response.status).toBe(500);
+    expect(response.body.error).toBe('insert or update on table \"Applies\" violates foreign key constraint \"Applies_userId_fkey\"');
+  });
+});
