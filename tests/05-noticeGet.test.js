9b10e840f80a94fd0fc8aa045cda47f756717e3c
cod2048 <cod2048@gmail.com>
2023-10-12 11:17:53 +0900

    [생성]채용공고 조회 test code

---
+++ tests/05-noticeGet.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Notice Data Check', () => {
+  it('should get all notice data', async () => {
+    const response = await request(app)
+      .get('/notice');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(6);
+  });
+});
